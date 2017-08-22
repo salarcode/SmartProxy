6a1804a2e8f648101655822b12d455d522a7f84c
salarcode <salar2k@gmail.com>
2017-08-22 09:00:58 +0430

    Added license notice to files.

--- src/core.js
+++ src/core.js
@@ -1,3 +1,19 @@
+/*
+ * This file is part of SmartProxy <https://github.com/salarcode/SmartProxy>,
+ * Copyright (C) 2017 Salar Khalilzadeh <salar2k@gmail.com>
+ *
+ * SmartProxy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * SmartProxy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SmartProxy.  If not, see <http://www.gnu.org/licenses/>.
+ */
 "use strict";
 const changesRerquireRestart = true;
 const proxyServerProtocols = ["HTTP", "HTTPS", "SOCKS4", "SOCKS5"];
