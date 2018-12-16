d9a4593558851359a92ffc0f1adba9d1c6b4c63e
salarcode <salar2k@gmail.com>
2018-12-16 15:15:04 +1100

    Initial lib files
    environment.ts , PolyFill.ts , Utils.ts

---
+++ src/lib/Utils.ts
@@ -0,0 +1,196 @@
+﻿import { browser, jQuery} from "./environment";
+
+export class Utils {
+	public static removeDuplicates(originalArray: string[], prop: string) {
+		//<reference path="https://stackoverflow.com/a/36744732/322446"/>
+		return originalArray.filter(
+			(thing, index, self) => self.findIndex((t) => {
+				return t[prop] === thing[prop];
+			}) === index);
+	}
+
+	public static removeDuplicatesFunc(originalArray: any[], areEqualFunc: Function) {
+		//<reference path="https://stackoverflow.com/a/36744732/322446"/>
+		return originalArray.filter(
+			(thing, index, self) => self.findIndex((t) => {
+				return areEqualFunc(t, thing);
+			}) === index);
+	}
+
+	public static strStartsWith(str, prefix) {
+		return str.substr(0, prefix.length) === prefix;
+	}
+
+	public static chunkString(str: string, length: number) {
+		let index = 0;
+		let endIndex = length;
+		if (endIndex > str.length)
+			endIndex = str.length;
+
+		let result = new Array<string>();
+		for (; ;) {
+			result.push(str.slice(index, endIndex));
+
+			if (endIndex >= str.length)
+				break;
+
+			index += length;
+			endIndex += length;
+
+			if (endIndex > str.length)
+				endIndex = str.length;
+		}
+
+		return result;
+	}
+
+	public static b64EncodeUnicode(str: string): string {
+		// first we use encodeURIComponent to get percent-encoded UTF-8,
+		// then we convert the percent encodings into raw bytes which
+		// can be fed into btoa.
+		return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+			(match, p1: number) => String.fromCharCode(p1)));
+	}
+
+	public static b64DecodeUnicode(str: string): string {
+		// Going backwards: from bytestream, to percent-encoding, to original string.
+		return decodeURIComponent(atob(str)
+			.split("")
+			.map(c => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+			.join(""));
+	}
+
+	public static isValidHost(host: string) {
+		return (host && host.indexOf("about:") === -1);
+	}
+
+	public static isValidUrl(url: string) {
+		try { new URL(url); return true; }
+		catch (e) { return false; }
+	}
+
+	public static urlHasSchema(url): boolean { // note: this will accept like http:/example.org/ in Chrome and Firefox
+		if (!url)
+			return false;
+		if (url.indexOf(":/") > -1)
+			return true;
+		return false;
+	}
+
+
+	public static extractHostFromUrl(url: string): string | null {
+		try {
+			const u = new URL(url);
+			const skip = ["moz-extension:", "chrome-extension:", "about:", "chrome:", "opera:"];
+			if (skip.indexOf(u.protocol) >= 0)
+				return null;
+			let host = u.host;
+
+			if (host.startsWith("www."))
+				return host.substring(4, host.length);
+
+			return host;
+		}
+		catch (e) { return null; }
+	}
+
+	public static extractSubdomainsFromUrl(url: string): string[] {
+		let host = Utils.extractHostFromUrl(url);
+		if (host === null)
+			return [];
+
+		return Utils.extractSubdomainsFromHost(host);
+	}
+
+	public static extractSubdomainsFromHost(host: string): string[] {
+		let parts = host.split(".");
+		if (parts.length <= 2)
+			return [host];
+
+		if (parts[0] === "www")
+			parts.splice(0, 1);
+
+		if (parts.length <= 2)
+			return [parts.join(".")];
+
+		let result = new Array<string>();
+		for (let i = 0; i < parts.length; i++) {
+			if (i == parts.length - 1)
+				break;
+
+			let sliced = parts.slice(i, parts.length);
+			//if (sliced.length > 0)
+			result.push(sliced.join("."));
+		}
+
+		result.reverse();
+		return result;
+	}
+
+	public static hostToMatchPattern(host: string): string {
+
+		// only convert to match pattern if it is just host address like 'google.com'
+		if (host.indexOf(":") > -1)
+			return host;
+
+		return `*://*.${host}/*`;
+	}
+
+	public static matchPatternToRegExp(pattern: string): RegExp | null {
+		// Source: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Match_patterns
+		// Modified by Salar Khalilzadeh
+		/**
+		 * Transforms a valid match pattern into a regular expression
+		 * which matches all URLs included by that pattern.
+		 *
+		 * @param  {string}  pattern  The pattern to transform.
+		 * @return {RegExp}           The pattern's equivalent as a RegExp.
+		 * @throws {TypeError}        If the pattern is not a valid MatchPattern
+		 */
+
+		// matches all valid match patterns (except '<all_urls>')
+		// and extracts [ , scheme, host, path, ]
+		const matchPattern = (/^(?:(\*|http|https|file|ftp|app):\/\/([^/]+|)\/?(.*))$/i);
+
+		if (pattern === "<all_urls>") {
+			//return (/^(?:https?|file|ftp|app):\/\//);
+			return null;
+		}
+		const match = matchPattern.exec(pattern);
+		if (!match) {
+			//throw new TypeError(`"${pattern}" is not a valid MatchPattern`);
+			return null;
+		}
+		const [, scheme, host, path,] = match;
+
+		return new RegExp("^(?:"
+			+ (scheme === "*" ? "https?" : escape(scheme)) + ":\\/\\/"
+			+ (host === "*" ? "[^\\/]*" : escape(host).replace(/^\*\./g, "(?:[^\\/]+)?"))
+			+ (path ? (path == "*" ? "(?:\\/.*)?" : ("\\/" + escape(path).replace(/\*/g, ".*"))) : "\\/?")
+			+ ")$");
+	}
+
+	public static localizeHtmlPage() {
+
+		function replace_i18n(obj, tag) {
+			let msg = browser.i18n.getMessage(tag.trim());
+
+			if (msg && msg != tag) obj.innerHTML = msg;
+		}
+
+		// page direction
+		let dir = browser.i18n.getMessage("uiDirection");
+		if (dir) {
+			jQuery(document.body).addClass(dir).css("direction", dir);
+		}
+
+		// Localize using data-localize tags
+		let data = window.document.querySelectorAll<HTMLElement>("[data-localize]");
+
+		data.forEach(obj => {
+			let tag = obj.dataset["localize"];
+
+			replace_i18n(obj, tag);
+		});
+	}
+}
---
+++ src/lib/polyfill.ts
@@ -0,0 +1,264 @@
+﻿import { environment, chrome, browser } from "./environment";
+
+export class PolyFill {
+	public static lastError() {
+		if (environment.chrome) {
+			// chrome.extension.lastError Deprecated since Chrome 58
+			return chrome.runtime.lastError;
+		} else {
+			return browser.runtime.lastError;
+		}
+	}
+	public static onProxyError() {
+		if (environment.chrome) {
+			return chrome.proxy.onProxyError;
+		} else {
+			if (browser.proxy.onError)
+				// this is under consideration for future version of Firefox #1388619
+				return browser.proxy.onError;
+			else
+				return browser.proxy.onProxyError;
+		}
+	}
+	public static tabsGet(tabId, success, fail) {
+		if (environment.chrome) {
+			chrome.tabs.get(tabId,
+				tabInfo => {
+					const error = PolyFill.lastError();
+					if (error) {
+						if (fail)
+							fail(error);
+					} else {
+						if (success)
+							success(tabInfo);
+					}
+				});
+		} else {
+			browser.tabs.get(tabId)
+				.then(success, fail);
+		}
+	}
+	public static tabsRemove(tabIds, success, fail) {
+		if (environment.chrome) {
+			chrome.tabs.remove(tabIds,
+				tabInfo => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail)
+							fail(error);
+					} else {
+						if (success)
+							success(tabInfo);
+					}
+				});
+		} else {
+			browser.tabs.remove(tabIds)
+				.then(success, fail);
+		}
+	}
+
+	public static tabsReload(tabId, success, fail, reloadProperties) {
+		if (environment.chrome) {
+			chrome.tabs.reload(tabId, reloadProperties,
+				tabInfo => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(tabInfo);
+					}
+				});
+		} else {
+			browser.tabs.reload(tabId, reloadProperties)
+				.then(success, fail);
+		}
+	}
+	public static tabsQuery(queryInfo, success, fail) {
+		if (environment.chrome) {
+			chrome.tabs.query(queryInfo,
+				tabs => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(tabs);
+					}
+				});
+		} else {
+			browser.tabs.query(queryInfo)
+				.then(success, fail);
+		}
+	}
+	public static tabsCreate(createProperties, success, fail) {
+		if (environment.chrome) {
+			chrome.tabs.create(createProperties,
+				tabInfo => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(tabInfo);
+					}
+				});
+		} else {
+			browser.tabs.create(createProperties)
+				.then(success, fail);
+		}
+	}
+	public static runtimeSendMessage(message, success, fail, options, extensionId) {
+		if (environment.chrome) {
+			if (options != null) {
+				// deleting firefox specific property of sending message to PAC
+				delete options["toProxyScript"];
+			}
+			chrome.runtime.sendMessage(extensionId,
+				message,
+				options,
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.runtime.sendMessage(
+				extensionId,
+				message,
+				options
+			).then(success, fail);
+		}
+	}
+	public static managementGetSelf(success, fail) {
+		if (environment.chrome) {
+			chrome.management.getSelf(
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.management.getSelf()
+				.then(success, fail);
+		}
+	}
+	public static storageLocalGet(keys, success, fail) {
+		if (environment.chrome) {
+			chrome.storage.local.get(keys,
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.storage.local.get(keys)
+				.then(success, fail);
+		}
+	}
+	public static storageLocalSet(items, success, fail) {
+		if (environment.chrome) {
+			chrome.storage.local.set(items,
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.storage.local.set(items)
+				.then(success, fail);
+		}
+	}
+	public static storageSyncGet(keys, success, fail) {
+		if (environment.chrome) {
+			chrome.storage.sync.get(keys,
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.storage.sync.get(keys)
+				.then(success, fail);
+		}
+	}
+	public static storageSyncSet(items, success, fail) {
+		if (environment.chrome) {
+			chrome.storage.sync.set(items,
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.storage.sync.set(items)
+				.then(success, fail);
+		}
+	}
+	public static runtimeGetBrowserInfo(success, fail) {
+		if (environment.chrome) {
+			// No implemented in chrome yet!
+			if (fail) fail({ message: "getBrowserInfo is not implemented" });
+
+			//chrome.runtime.getBrowserInfo(
+			//	function (response) {
+			//		const error = polyfill.lastError();
+			//		if (error) {
+			//			if (fail) fail(error);
+			//		} else {
+			//			if (success) success(response);
+			//		}
+			//	});
+		} else {
+			browser.runtime.getBrowserInfo()
+				.then(success, fail);
+		}
+	}
+	public static runtimeOpenOptionsPage(success, fail) {
+		if (environment.chrome) {
+			chrome.runtime.openOptionsPage(
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.runtime.openOptionsPage()
+				.then(success, fail);
+		}
+	}
+	public static browserActionSetIcon(details, success, fail) {
+		if (environment.chrome) {
+			chrome.browserAction.setIcon(details,
+				response => {
+					let error = PolyFill.lastError();
+					if (error) {
+						if (fail) fail(error);
+					} else {
+						if (success) success(response);
+					}
+				});
+		} else {
+			browser.browserAction.setIcon(details)
+				.then(success, fail);
+		}
+	}
+}
