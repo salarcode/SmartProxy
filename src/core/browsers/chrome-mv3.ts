d6fad8fdc2b68e0a1c1be3c1e3cc27c8b96bfc2b
salarcode <salar2k@gmail.com>
2022-09-06 00:19:11 +1000

    Allowing builds for both MV3 and MV2 for Chrome

---
+++ src/core/browsers/chrome-mv3.ts
@@ -0,0 +1,7 @@
+import { environment } from "../../lib/environment";
+
+environment.browserConfig = {
+    name: "Chrome",
+    marketName: "Chrome Web Store",
+    marketUrl: "https://chrome.google.com/webstore/detail/smartproxy/jogcnplbkgkfdakgdenhlpcfhjioidoj"
+};
