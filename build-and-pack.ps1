0e86169cd0438c6ae15985d54936f0ee977a0984
salarcode <salar2k@gmail.com>
2023-02-02 00:26:42 +1100

    Powershell script to create release packs

---
+++ build-and-pack.ps1
@@ -0,0 +1,46 @@
+# run this in powershell to enable scripts
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+$global:Success = $true
+$global:version = ""
+
+Write-Output "Creating Browser Markets Release Packs..."
+
+Function BuildAndPack($Build, $BuildName) {
+
+ if(!$global:Success) {
+    return;
+ }
+ Write-Output "Cleaning build folder"
+ Remove-Item -path .\build\* -recurse
+
+ Invoke-Expression ("npm run build-$Build " + ';$global:Success=$?')
+
+ if($global:Success)
+ {
+    $manifest = Get-Content .\build\manifest.json | ConvertFrom-Json
+    $global:version = $manifest.version
+    $zipfile = "..\!Releases\SmartProxy-v$global:version-$BuildName.zip"
+    $compress = @{
+        Path = ".\build\*"
+        DestinationPath = $zipfile
+    }
+    Compress-Archive @compress
+    Write-Output "Created release in $zipfile"
+ }
+ else
+ {
+    Write-Output "Build has failed for $BuildName"
+ }
+}
+
+
+BuildAndPack "ff" "Firefox"
+BuildAndPack "ch"  "Chrome-ManifestV3"
+BuildAndPack "ed"  "Edge"
+BuildAndPack "th"  "Thunderbird"
+BuildAndPack "op"  "Opera"
+BuildAndPack "ff-unlisted" "firefox-unlisted"
+
+Invoke-Expression ("git archive --format zip -o ..\!Releases\SmartProxy-$global:version-sources.zip HEAD")
+Write-Output "Sources are saved in SmartProxy-$global:version-sources.zip"
